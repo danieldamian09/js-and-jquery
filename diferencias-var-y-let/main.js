e842aa7fa0d025a0f57c2baa2abc7d1b261baedd
danieldamian09 <humbertorivero9@gmail.com>
2021-11-13 12:47:58 -0300

    diferencia entre var y let ejemplos

---
+++ diferencias-var-y-let/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var numero = 20;
+
+var texto = "hola";
+
+if(true){
+  let numero = 40;
+  let texto = "Hola que tal";
+  console.log(numero);
+  console.log(texto);
+}
+
+console.log(numero);
+console.log(texto);
+
