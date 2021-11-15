6bb9c78d5dc91dbfd02238d0daa316c9cfa0f563
danieldamian09 <humbertorivero9@gmail.com>
2021-11-15 11:51:02 -0300

    ejercios del ciclo for

---
+++ bucle-for/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Bucle for
+// Dentro del bucle tenemos 3 partes
+// Inicializacion var i = 0;
+// Evaluacion i <= 100;
+// Iteracion i++;
+
+for(var i = 0; i <= 100; i++){
+  console.log(i);
+}
