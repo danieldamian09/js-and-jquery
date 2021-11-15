43c0056f4a6784cb8009f3aed45b30c7b24303c0
danieldamian09 <humbertorivero9@gmail.com>
2021-11-15 10:13:39 -0300

    ejercios de operador matematiocos

---
+++ operadores-matematicos/main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var numero = 1;
+var numero2 = 5;
+
+// suma
+var resultadoSuma = numero + numero2;
+// resta
+var resultadoResta = numero - numero2;
+// division 
+var resultadodivision = numero / numero2;
+// multiplicacion
+var resultadomultiplicacion = numero * numero2;
+
+console.log(`El resultado de la suma es: ${resultadoSuma}`);
+console.log(`El resultado de la resta es: ${resultadoResta}`);
+console.log(`El resultado de la division es: ${resultadodivision}`);
+console.log(`El resultado de la multiplicacion es: ${resultadomultiplicacion}`);
+
+resultadoSuma++;
+console.log(`incrementamos++ ${resultadoSuma}`);
+resultadoSuma--;
+console.log(`restamos-- ${resultadoSuma}`)
+
+
+
