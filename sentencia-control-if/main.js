a1daadf0530f7362182c1e931c72787867b6d9d8
danieldamian09 <humbertorivero9@gmail.com>
2021-11-15 10:31:32 -0300

    sentencia de control if

---
+++ sentencia-control-if/main.js
@@ -0,0 +1,14 @@
+var edad = 19;
+
+if (edad > 18) {
+  console.log("Eres mayor de Edad");
+} else {
+  console.log("Eres menor de Edad");
+}
+
+if (edad > 18 && edad < 30) {
+  console.log("Estas entre 19 y 30 años")
+} else {
+  console.log("Muy Joven o demasiado Mayor 😁")
+}
+
