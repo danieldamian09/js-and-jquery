0a6647c3224bbab51d1a139a4d1285fc39371a0e
danieldamian09 <humbertorivero9@gmail.com>
2021-11-22 20:25:44 -0300

    cambios en la estructura

---
+++ javascript_basic/bucle-do-while/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var dato = 1;
+
+do {
+  console.log("el valor de dato es " + dato);
+  dato++; //simpre se debe hacer el incrimento
+} while (dato <= 5);
+
+console.log("salimos del bucle");
