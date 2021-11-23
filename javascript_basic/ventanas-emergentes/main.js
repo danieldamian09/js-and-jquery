aa05eeb1f5e7639cbed0256a13ff0d4306c3dd5f
danieldamian09 <humbertorivero9@gmail.com>
2021-11-22 21:00:35 -0300

    como utilizar las ventanas del navegador en js

---
+++ javascript_basic/ventanas-emergentes/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Alertas dentro del navegador
+alert("mensaje de alerta dentro del navegador");
+
+// Ventanas de confirmacion dentro del navegador;
+var valorConfirm = confirm("aceptas los terminos");
+// Para capturar el valor de la ventana confirm que puede ser Falso o True utilizamos la variable valorConfirm;
+alert(valorConfirm);
+
+// Ventana de ingreso de Datos dentro del Navegador
+var valorPrompt = prompt("ingrese un numero");
+alert("El valor ingresado es " + valorPrompt);
+
+
