5cc4c8ff9fc6093455624ae2f6dff5d8ad53bcfb
danieldamian09 <humbertorivero9@gmail.com>
2021-11-24 09:18:01 -0300

    ejemplos de funciones

---
+++ javascript_basic/funciones/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function nombreFunction(){
+  return "Hola soy el return de la primera funcion";
+}
+
+function mostrarMensaje(){
+  console.log("hola que tal estoy dentro de la funcion");
+}
+
+mostrarMensaje();
